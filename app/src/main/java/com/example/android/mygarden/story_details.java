262f591b17f3cb78fab662038c45cc6ebe4155f7
mohammedAlefrangy <mohamed.alefrangy@gmail.com>
2018-10-27 12:23:23 +0200

    add stroy details and shar button

---
+++ app/src/main/java/com/example/android/mygarden/story_details.java
@@ -0,0 +1,35 @@
+package com.example.android.mygarden;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class story_details extends AppCompatActivity {
+
+    TextView details , nameOfPreson ;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_story_details);
+        details = (TextView)findViewById(R.id.details);
+        nameOfPreson = (TextView)findViewById(R.id.nameOfPreson);
+
+    }
+
+
+    public void onBackButtonClick(View view) {
+        finish();
+    }
+
+    public void onSharClick(View view) {
+        String message = details.getText().toString();
+        String name = nameOfPreson.getText().toString();
+        Intent share = new Intent(Intent.ACTION_SEND);
+        share.setType("text/plain");
+        share.putExtra(Intent.EXTRA_TEXT, message);
+
+        startActivity(Intent.createChooser(share, name));
+    }
+}
--- app/src/main/java/com/example/android/mygarden/story_list.java
+++ app/src/main/java/com/example/android/mygarden/story_list.java
@@ -1,5 +1,6 @@
 package com.example.android.mygarden;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -15,6 +16,8 @@ public class story_list extends AppCompatActivity {
     }
 
     public void onStoryCliced(View view) {
+        Intent intent = new Intent(story_list.this, story_details.class);
+        startActivity(intent);
     }
 
 
